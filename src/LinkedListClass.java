0c5f736aadaefcd31ed06b8192aca15da07bb608
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-04 16:15:41 +0530

    Class Node

--- src/LinkedListClass.java
+++ src/LinkedListClass.java
@@ -2,5 +2,12 @@ public class LinkedListClass {
     Node head;
     Node second;
     Node third;
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+        }
+    }
 
 }
