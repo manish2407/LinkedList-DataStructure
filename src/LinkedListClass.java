94ef5179bd13dd77824f619f8a6551cdf429acff
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-09 22:31:09 +0530

    middlevalue function finish

--- src/LinkedListClass.java
+++ src/LinkedListClass.java
@@ -160,6 +160,12 @@ public class LinkedListClass {
         while(current !=null){
             length = length+1;
             current = current.next;
+        }current = head;
+        if(length%2!=0) {
+            for (int i = 1; i <= length / 2; i++) {
+                current = current.next;
+            }
+            System.out.println("\nvalue of middle node is " + current.data);
         }
         else{
             current = head;
