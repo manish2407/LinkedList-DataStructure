ce049e9525b551739d5be82cb8c67d6547698b01
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-10 14:40:40 +0530

    class node with constructor

--- src/InsertionSortOnLInkedLIst.java
+++ src/InsertionSortOnLInkedLIst.java
@@ -1,6 +1,13 @@
 public class InsertionSortOnLInkedLIst {
     Node head;
 
-
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
 
 }
