67baa38d0c1de15293186f2484b34dc05b63b2aa
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-11 08:20:17 +0530

    main method

--- src/DoublyLinkedList/DoublyLinkedListClass.java
+++ src/DoublyLinkedList/DoublyLinkedListClass.java
@@ -1,15 +1,24 @@
 package DoublyLinkedList;
 
 public class DoublyLinkedListClass {
-    Node head;
+    Node head;      //head node
+
+    //Node class
     class Node{
         int data;
         Node previous;
         Node next;
+
+        //constructor
         public Node(int d){
             data = d;
             previous=null;
             next=null;
         }
     }
+
+    public static void main(String[] args) {
+
+    }
+
 }
