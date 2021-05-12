84dd64e2ba5784d9b997f19e91b44cd32317de13
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-05-12 23:01:49 +0530

    insert node before head

--- src/DoublyLinkedList/DoublyLinkedListClass.java
+++ src/DoublyLinkedList/DoublyLinkedListClass.java
@@ -19,6 +19,8 @@ public class DoublyLinkedListClass {
     //1. insert node at head in doubly linked list
     public  void insertAtFirst(int data){
         Node newNode = new Node(data);
+        newNode.next = head;
+
     }
 
     public static void main(String[] args) {
